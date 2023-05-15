c01b4b3a00ee27ddaf2b8e5c135abac0f8768ea7
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-05-15 13:06:12 +0530

    AddingTsk

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+const taskBtn = document.getElementById("add");
+taskBtn.addEventListener("click", addTask);
 
+const textValue = document.getElementById("inputBox");
+
+var arr = JSON.parse(localStorage.getItem("todos")) || [];
+
+function addTask(){
+
+arr.push({task : textValue.value});
+localStorage.setItem("todos" , JSON.stringify(arr));
+
+displayData(arr)
+}
+
+function displayData(data){
+    document.getElementById("TodoTask").innerHTML = "";
+    data.map((el) => {
+        var tr = document.createElement("tr");
+        let td = document.createElement("td");
+        td.innerText = el.task;
+
+       var btn1 = document.createElement("button");
+       btn1.innerText = "Edit";
+
+       var btn2 = document.createElement("button");
+       btn2.innerText = "Delete";
+       
+       tr.append(td , btn1 , btn2);
+       document.querySelector("#TodoTask").append(tr);
+    })
+}
+
+displayData(arr)
